60808c40d8543a6e0cf91ed70807f1a0ee4f6f13
DrLivesee <evgenijmanaenkov@gmail.com>
2021-08-23 15:28:46 +0300

    start footer

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,10 @@ import '../css/common.scss'
 import '../css/header.scss'
 import '../css/about-us.scss'
 import '../css/works.scss'
+import '../css/cooperation.scss'
+import '../css/contacts.scss'
 
 // Import JS files
 import './aos'
 import './module'
 import './scroll'
-import './opacity'
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,20 +2,108 @@ const people = document.querySelector('.people')
 const peopleFix = document.querySelector('.people-fixed')
 const madeFix = document.querySelector('.made-fixed')
 const cliqueFix = document.querySelector('.clique-fixed')
+const fullName = document.querySelector('.main-name')
+const textAboutUs = document.querySelector('.about-us .text')
+const textWorks = document.querySelector('.works .text')
+const textCooperation = document.querySelector('.cooperation .text')
+
+const VERT_OFFSET = 200
+
+const opacMiddleTitle = (
+	scrollPos,
+	minVisPos,
+	maxVisPos,
+	titleNode,
+	isFirst,
+	isLast
+) => {
+	if (scrollPos <= minVisPos + (maxVisPos - minVisPos) / 4) {
+		if (isFirst) {
+			titleNode.style.opacity = 1
+		} else {
+			titleNode.style.opacity =
+				(scrollPos - minVisPos) / ((maxVisPos - minVisPos) / 4)
+		}
+	} else if (scrollPos >= maxVisPos - (maxVisPos - minVisPos) * 0.75) {
+		if (isLast) {
+			titleNode.style.opacity = 1
+		} else {
+			titleNode.style.opacity =
+				(maxVisPos - scrollPos) / ((maxVisPos - minVisPos) / 4)
+		}
+	} else {
+		titleNode.style.opacity = 1
+	}
+}
 
 window.addEventListener('scroll', function () {
 	let scroll = window.pageYOffset
-	if (scroll === 0) {
-		people.classList.remove('display-none')
+
+	console.log(scroll)
+	// const bodyHeight = document.body.clientHeight
+
+	opacMiddleTitle(scroll, -200, 760 - VERT_OFFSET, fullName, false, false)
+	opacMiddleTitle(scroll, 500, 1900 - VERT_OFFSET, textAboutUs, false, false)
+	opacMiddleTitle(scroll, 1500, 2750 - VERT_OFFSET, textWorks, false, false)
+	opacMiddleTitle(
+		scroll,
+		2500,
+		4000 - VERT_OFFSET,
+		textCooperation,
+		false,
+		false
+	)
+
+	opacMiddleTitle(scroll, 0, 1700 - VERT_OFFSET, peopleFix, true, false)
+	opacMiddleTitle(
+		scroll,
+		1695 - VERT_OFFSET,
+		2700 - VERT_OFFSET,
+		madeFix,
+		false,
+		false
+	)
+	opacMiddleTitle(
+		scroll,
+		2695 - VERT_OFFSET,
+		3550 - VERT_OFFSET,
+		cliqueFix,
+		false,
+		false
+	)
+
+	if (scroll > 1495) {
+		madeFix.classList.add('z-index')
 	} else {
+		madeFix.classList.remove('z-index')
+	}
+
+	if (scroll > 2495) {
+		madeFix.classList.remove('z-index')
+		cliqueFix.classList.add('z-index')
+	} else {
+		cliqueFix.classList.remove('z-index')
+	}
+
+	if (scroll > 0) {
+		people.classList.add('display-none')
+		peopleFix.classList.remove('display-none')
+	} else {
+		people.classList.remove('display-none')
+		peopleFix.classList.add('display-none')
+	}
+
+	if (scroll === 0) {
 		people.classList.add('display-none')
 	}
 
-	if (scroll < 1505) {
-		peopleFix.classList.remove('display-none')
-		madeFix.classList.add('display-none')
-	} else {
+	if (scroll > 3350) {
 		peopleFix.classList.add('display-none')
+		madeFix.classList.add('display-none')
+		cliqueFix.classList.add('display-none')
+	} else {
+		peopleFix.classList.remove('display-none')
 		madeFix.classList.remove('display-none')
+		cliqueFix.classList.remove('display-none')
 	}
 })

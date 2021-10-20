bb966ef48122c8880eab4ab15f38a340b367b6ce
DrLivesee <evgenijmanaenkov@gmail.com>
2021-10-20 18:45:29 +0300

    test safari n1

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -114,46 +114,46 @@ window.addEventListener('scroll', function () {
 })
 
 
-// $(document).ready(function(){
-// 	$(people).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
+$(document).ready(function(){
+	$(people).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
 
-// 	$(made).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
+	$(made).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
 
-// 	$(clique).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
+	$(clique).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
 
-// 	$(peopleFix).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
+	$(peopleFix).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
 
-// 	$(madeFix).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
+	$(madeFix).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
 
-// 	$(cliqueFix).click(function (e) {
-// 		e.preventDefault();
-// 		var id  = $(this).attr('href'),
-// 			top = $(id).offset().top;
-// 		$('body,html').animate({scrollTop: top}, 500);
-// 	});
-// });
+	$(cliqueFix).click(function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+});
